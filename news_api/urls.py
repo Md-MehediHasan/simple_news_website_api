34f53f7d1820623a3b683911bf90b11ff4739bd5
Md. Mehedi Hasan <lmehedi327@gmail.com>
2024-03-16 00:20:46 +0600

    Final commit

--- news_api/urls.py
+++ news_api/urls.py
@@ -1,16 +1,19 @@
 
 from django.contrib import admin
+from django.conf.urls import url
 from django.conf import settings
+from django.views.static import serve
 from django.conf.urls.static import static
 from django.urls import path,include
 
 
 urlpatterns = [
+     url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
     path('api/',include('api.url')),
   
    
 
 ]
-if not settings.DEBUG:
-    urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+
